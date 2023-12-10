5d57f073e1017e215600f8b0d76cbffa53119ff4
Nounail <122325232+Nounail@users.noreply.github.com>
2023-12-09 23:46:27 -0500

    Change 4
    Change 4

---
+++ UI/Main_Menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _ready():
+	get_tree().paused = false
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://UI/HOWTOPLAY.tscn")
+
